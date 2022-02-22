ff5a8f43ad1335d816b80b3d08b97d7e8c2c52e4
SihleManaka <johannesmanaka71@gmail.com>
2022-02-22 21:34:37 +0200

    Initial commit

---
+++ src/main/java/com/example/springboot/testing/tutorial/SpringbootTestingTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.testing.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestingTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestingTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot/testing/tutorial/SpringbootTestingTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.testing.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestingTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
