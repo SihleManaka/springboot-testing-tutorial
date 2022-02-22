b1aabf2f39e0b8c13ef13a5eff0b3f8542aa43eb
SihleManaka <johannesmanaka71@gmail.com>
2022-02-22 22:20:49 +0200

    Section 2: Spring boot project setup

---
+++ src/main/java/com/example/springboot/testing/tutorial/model/Employee.java
@@ -0,0 +1,27 @@
+package com.example.springboot.testing.tutorial.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Builder
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+}
